538cb0f41a78b5bfa48986c10ee6270d97e51d52
fcheskagm <gonzalezf21@ujap.edu.ve>
2024-05-15 20:35:15 -0400

    Conversiones

--- evaluacion_1/funciones.py
+++ evaluacion_1/funciones.py
@@ -1,133 +1,205 @@
 class Conversiones:
     def __init__(self):        
-        self.decimal = 0.0
+        self.decimal = 0
         self.binario = ""
         self.terciario = ""
         self.cuaternario = ""
         self.octal = ""
         self.hexadecimal = ""
+        self.lista = ["Decimal","Binario","Terciario","Cuaternario","Octal","Hexadecimal"]
 
     def get_decimal(self):
         return self.decimal
-    def set_decimal(self, decimal):
-        self.decimal = decimal
+    def set_decimal(self, decimal=0):
+        self.decimal = int(decimal)
 
     def get_binario(self):
         return self.binario
-    def set_binario(self, binario):
+    def set_binario(self, binario=""):
         self.binario = binario
 
     def get_terciario(self):
         return self.terciario
-    def set_terciario(self, terciario):
+    def set_terciario(self, terciario=""):
         self.terciario = terciario
 
     def get_cuaternario(self):
         return self.cuaternario
-    def set_cuaternario(self, cuaternario):
+    def set_cuaternario(self, cuaternario=""):
         self.cuaternario = cuaternario
 
     def get_octal(self):
         return self.octal
-    def set_octal(self, octal):
+    def set_octal(self, octal=""):
         self.octal = octal
 
     def get_hexadecimal(self):
         return self.hexadecimal
-    def set_hexadecimal(self, hexadecimal):
+    def set_hexadecimal(self, hexadecimal=""):
         self.hexadecimal = hexadecimal
             
-    def decimal_binario(self):
-        if self.binario == "":
-            if self.decimal == 0:
-                return "0"
+    def todas_conversiones(self,value1,value2):
+        if value1 == "Binario" and value2 == "Decimal":
+            resultado_decimal = int(self.binario, 2)
+            self.decimal = resultado_decimal
 
+        elif value1 == "Decimal" and value2 == "Binario":
+            if self.decimal == 0:
+                self.binario = "0"
             resultado_binario = ""
             self.decimal = int(self.decimal)
             while self.decimal > 0:
                 residuo = self.decimal % 2
                 resultado_binario = str(residuo) + resultado_binario
                 self.decimal //= 2
-            return resultado_binario
+            self.binario = resultado_binario
+
+        elif value1 == "Decimal" and value2 == "Terciario":
+            res_terciario = ""
+            while self.decimal > 0:
+                resto = self.decimal % 3
+                res_terciario = str(resto) + res_terciario
+                self.decimal //= 3
+            self.terciario = res_terciario
         
-        elif self.decimal == 0:
-            resultado_decimal = int(self.binario, 2)
-            return resultado_decimal
+        elif value1 == "Terciario" and value2 == "Decimal":
+            res_decimal = 0
+            for i, digito in enumerate(reversed(self.terciario)):
+                res_decimal += int(digito) * (3 ** i)
+            self.decimal = res_decimal
 
-    def decimal_terciario(self):        
-        if self.decimal == 0:
-            terciario = ""
-            while decimal > 0:
-                resto = decimal % 3
-                terciario = str(resto) + terciario
-                decimal //= 3
+        elif value1 == "Decimal" and value2 == "Cuaternario":
+            res_cuaternario = ""
+            while self.decimal > 0:
+                resto = self.decimal % 4
+                cuaternario = str(resto) + res_cuaternario
+                self.decimal //= 4
+            self.cuaternario = res_cuaternario
 
-            return terciario
-        else:
-            decimal = 0
-            for i, digito in enumerate(reversed(terciario)):
-                decimal += int(digito) * (3 ** i)
+        elif value1 == "Cuaternario" and value2 == "Decimal":
+            res_decimal = 0
+            for i, digito in enumerate(reversed(self.cuaternario)):
+                res_decimal += int(digito) * (4 ** i)
+            self.decimal = res_decimal
 
-            return decimal
+        elif value1 == "Decimal" and value2 == "Octal":
+            res_octal = ""
+            while self.decimal > 0:
+                residuo = self.decimal % 8
+                res_octal = str(residuo) + res_octal
+                self.decimal //= 8
+            self.octal = res_octal
 
-    def decimal_cuaternario(self):        
-        if self.decimal == 0:
-            cuaternario = ""
-            while decimal > 0:
-                resto = decimal % 4
-                cuaternario = str(resto) + cuaternario
-                decimal //= 4
-
-            return cuaternario
-        else:
-            decimal = 0
-            for i, digito in enumerate(reversed(cuaternario)):
-                decimal += int(digito) * (4 ** i)
-
-            return decimal
-
-    def decimal_octal(self):        
-        if self.decimal == 0:
-            octal = ""
-            while decimal > 0:
-                residuo = decimal % 8
-                octal = str(residuo) + octal
-                decimal //= 8
-
-            return octal
-        else:
+        elif value1 == "Octal" and value2 == "Decimal":
             base = 1
-            for digito in reversed(octal):
-                decimal += int(digito) * base
+            res_decimal = 0
+            for digito in reversed(self.octal):
+                res_decimal += int(digito) * base
                 base *= 8
-
-            return decimal
+            self.decimal = res_decimal
         
-    def decimal_hexadecimal(self):
-        if self.decimal == 0:
-            hexadecimal = ""
-            while decimal > 0:
-                residuo = decimal % 16
+        elif value1 == "Decimal" and value2 == "Hexadecimal":
+            res_hexadecimal = ""
+            while self.decimal > 0:
+                residuo = self.decimal % 16
                 if residuo < 10:
-                    hexadecimal = str(residuo) + hexadecimal
+                    res_hexadecimal = str(residuo) + res_hexadecimal
                 else:
-                    hexadecimal = chr(ord('A') + residuo - 10) + hexadecimal
-                decimal //= 16
+                    res_hexadecimal = chr(ord('A') + residuo - 10) + res_hexadecimal
+                self.decimal //= 16
+            self.hexadecimal = res_hexadecimal
 
-            return hexadecimal
-        else:
-            decimal = 0
+        elif value1 == "Hexadecimal" and value2 == "Decimal":
+            res_decimal = 0
             base = 1
-            for digito in reversed(hexadecimal):
+            for digito in reversed(self.hexadecimal):
                 if digito.isdigit():
-                    decimal += int(digito) * base
+                    res_decimal += int(digito) * base
                 else:
-                    decimal += (ord(digito.upper()) - ord('A') + 10) * base
+                    res_decimal += (ord(digito.upper()) - ord('A') + 10) * base
                 base *= 16
+            self.decimal = res_decimal
 
-            return decimal
+        elif value1 == "Hexadecimal" and value2 == "Binario":
+            self.todas_conversiones("Hexadecimal","Decimal")
+            self.todas_conversiones("Decimal", "Binario")
 
+        elif value1 == "Binario" and value2 == "Hexadecimal":
+            self.todas_conversiones("Binario","Decimal")
+            self.todas_conversiones("Decimal", "Hexadecimal")    
 
-n = Conversiones("10100")
-binario = n.decimal_binario()
-print(binario)
+        elif value1 == "Binario" and value2 == "Terciario":
+            self.todas_conversiones("Binario", "Decimal")
+            self.todas_conversiones("Decimal", "Terciario")
+
+        elif value1 == "Terciario" and value2 == "Binario":
+            self.todas_conversiones("Terciario", "Decimal")
+            self.todas_conversiones("Decimal", "Binario")
+
+        elif value1 == "Binario" and value2 == "Cuaternario":
+            self.todas_conversiones("Binario", "Decimal")
+            self.todas_conversiones("Decimal", "Cuaternario")
+
+        elif value1 == "Cuaternario" and value2 == "Binario":
+            self.todas_conversiones("Cuaternario", "Decimal")
+            self.todas_conversiones("Decimal", "Binario")
+
+        elif value1 == "Binario" and value2 == "Octal":
+            self.todas_conversiones("Binario", "Decimal")
+            self.todas_conversiones("Decimal", "Octal")    
+
+        elif value1 == "Octal" and value2 == "Binario":
+            self.todas_conversiones("Octal", "Decimal")
+            self.todas_conversiones("Decimal", "Binario")    
+
+        elif value1 == "Cuaternario" and value2 == "Terciario":
+            self.todas_conversiones("Cuaternario", "Decimal")
+            self.todas_conversiones("Decimal", "Terciario")
+
+        elif value1 == "Terciario" and value2 == "Cuaternario":
+            self.todas_conversiones("Terciario", "Decimal")
+            self.todas_conversiones("Decimal", "Cuaternario")
+
+        elif value1 == "Octal" and value2 == "Terciario":
+            self.todas_conversiones("Octal", "Decimal")
+            self.todas_conversiones("Decimal", "Terciario")
+
+        elif value1 == "Terciario" and value2 == "Octal":
+            self.todas_conversiones("Terciario", "Decimal")
+            self.todas_conversiones("Decimal", "Octal")
+
+        elif value1 == "Hexadecimal" and value2 == "Terciario":
+            self.todas_conversiones("Hexadecimal","Decimal")
+            self.todas_conversiones("Decimal", "Terciario")
+
+        elif value1 == "Terciario" and value2 == "Hexadecimal":
+            self.todas_conversiones("Terciario", "Decimal")
+            self.todas_conversiones("Decimal", "Hexadecimal")
+
+        elif value1 == "Cuaternario" and value2 == "Octal":
+            self.todas_conversiones("Cuaternario", "Decimal")
+            self.todas_conversiones("Decimal", "Octal")
+
+        elif value1 == "Octal" and value2 == "Cuaternario":
+            self.todas_conversiones("Octal", "Decimal")
+            self.todas_conversiones("Decimal", "Cuaternario")
+
+        elif value1 == "Hexadecimal" and value2 == "Cuaternario":
+            self.todas_conversiones("Hexadecimal","Decimal")
+            self.todas_conversiones("Decimal", "Cuaternario")
+
+        elif value1 == "Cuaternario" and value2 == "Hexadecimal":
+            self.todas_conversiones("Cuaternario", "Decimal")
+            self.todas_conversiones("Decimal", "Hexadecimal")
+
+        elif value1 == "Hexadecimal" and value2 == "Octal":
+            self.todas_conversiones("Hexadecimal","Decimal")
+            self.todas_conversiones("Decimal", "Octal")
+
+        elif value1 == "Octal" and value2 == "Hexadecimal":
+            self.todas_conversiones("Octal", "Decimal")
+            self.todas_conversiones("Decimal", "Hexadecimal")
+
+        elif value1 == value2:
+            None 
+                    
--- evaluacion_1/interfaz.py
+++ evaluacion_1/interfaz.py
@@ -1,13 +1,29 @@
+from funciones import Conversiones
+import random
 import flet as ft
 
 def main(page: ft.Page):
-    def dropdown_changed(e):
-        t.value = f"Selecciona para cambiar la opcion de {seleccion.value}"
-        page.update()
 
-    t = ft.Text()
-    seleccion = ft.Dropdown(
-        on_change=dropdown_changed,
+    c=Conversiones()
+
+    page.window_bgcolor = ft.colors.WHITE
+    page.bgcolor = ft.colors.WHITE
+    page.vertical_alignment = ft.MainAxisAlignment.CENTER
+    page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
+
+    original_text_field = ft.TextField(
+        value="", 
+        text_align=ft.TextAlign.RIGHT,
+        color=ft.colors.BLACK
+        )
+    convertir_text_field = ft.TextField(
+        value="", 
+        text_align=ft.TextAlign.LEFT, 
+        disabled=True, 
+        bgcolor='#fbfbfb',
+        color=ft.colors.BLACK,
+        )
+    original_dropdown = ft.Dropdown(
         options=[
             ft.dropdown.Option("Decimal"),
             ft.dropdown.Option("Binario"),
@@ -17,11 +33,195 @@ def main(page: ft.Page):
             ft.dropdown.Option("Hexadecimal"),
         ],
         width=200,
+        bgcolor='#923fe5',
+        color='white',
+    )
+    convertir_dropdown = ft.Dropdown(
+        options=[
+            ft.dropdown.Option("Decimal"),
+            ft.dropdown.Option("Binario"),
+            ft.dropdown.Option("Terciario"),
+            ft.dropdown.Option("Cuaternario"),
+            ft.dropdown.Option("Octal"),
+            ft.dropdown.Option("Hexadecimal"),
+        ],
+        width=200,
+        bgcolor='#923fe5',
+        color='white',
     )
 
-    txt_number = ft.TextField(value="", text_align=ft.TextAlign.RIGHT, width=400)
+    def set_ori(value):
+        if value == "Binario":
+            c.set_binario(original_text_field.value)
+        elif value == "Decimal":
+            c.set_decimal(original_text_field.value)
+        elif value == "Terciario":
+            c.set_terciario(original_text_field.value)
+        elif value == "Cuaternario":
+            c.set_cuaternario(original_text_field.value)
+        elif value == "Octal":
+            c.set_octal(original_text_field.value)
+        elif value == "Hexadecimal":
+            c.set_hexadecimal(original_text_field.value)
 
+    def get_con(value):
+        if value == "Binario":
+            return c.get_binario()
+        elif value == "Decimal":
+            return c.get_decimal()
+        elif value == "Terciario":
+            return c.get_terciario()
+        elif value == "Cuaternario":
+            return c.get_cuaternario()
+        elif value == "Octal":
+            return c.get_octal()
+        elif value == "Hexadecimal":
+            return c.get_hexadecimal()
+        
+    def set_borrar():
+            c.set_binario()
+            c.set_decimal()
+            c.set_terciario()
+            c.set_cuaternario()
+            c.set_octal()
+            c.set_hexadecimal()
+    
 
-    page.add(t, seleccion, txt_number)
+    def boton_clicked(e):
+        valor_original = original_dropdown.value
+        valor_convertir = convertir_dropdown.value
+        if valor_original and valor_convertir:
+            if validar_valor_original(original_text_field.value, valor_original):
+                set_ori(valor_original)
+                c.todas_conversiones(valor_original,valor_convertir)
+                convertir_text_field.value = get_con(valor_convertir)
+        else:
+            convertir_text_field.value = "Valor no válido"
+        page.update()
+
+    def validar_valor_original(valor, base):
+        if base == "Decimal":
+            return all(c in "0123456789" for c in str(valor))
+        elif base == "Binario":
+            return all(c in "01" for c in valor)
+        elif base == "Terciario":
+            return all(c in "012" for c in valor)
+        elif base == "Cuaternario":
+            return all(c in "0123" for c in valor)
+        elif base == "Octal":
+            return all(c in "01234567" for c in valor)
+        elif base == "Hexadecimal":
+            return all(c in "0123456789ABCDEF" for c in valor.upper())
+        return False
+
+    def random_num(value):
+        if value == "Binario":
+            c.set_binario("".join(str(random.randint(0, 1)) for _ in range(random.randint(3, 10))))
+        elif value == "Decimal":
+            c.set_decimal(random.uniform(0,10000))
+        elif value == "Terciario":
+            c.set_terciario("".join(str(random.randint(0, 2)) for _ in range(random.randint(3, 6))))
+        elif value == "Cuaternario":
+            c.set_cuaternario("".join(str(random.randint(0, 3)) for _ in range(random.randint(3, 6))))
+        elif value == "Octal":
+            c.set_octal("".join(str(random.randint(0, 7)) for _ in range(random.randint(3, 6))))
+        elif value == "Hexadecimal":
+            c.set_hexadecimal("".join(random.choice("0123456789ABCDEF") for _ in range(random.randint(3, 6))))
+
+    def crear_container(content, **kwargs):
+        return ft.Container(content=content, **kwargs)
+
+    def eliminar_clicked(e):
+        original_text_field.value = ""
+        convertir_text_field.value = ""
+        original_dropdown.value = ""
+        convertir_dropdown.value = ""
+        set_borrar()  
+        page.update()
+
+    def click_random(e):
+        valor_original = original_dropdown.value
+        random_num(valor_original)
+        original_text_field.value = get_con(valor_original)
+        page.update()
+
+    page.add(
+        ft.Row(
+            [
+                ft.Column(
+                    [
+                        crear_container(
+                            original_dropdown, 
+                            alignment=ft.alignment.center, 
+                            width=400, 
+                            height=100, 
+                            bgcolor=ft.colors.WHITE),
+                        crear_container(
+                            original_text_field, 
+                            alignment=ft.alignment.center, 
+                            width=400, 
+                            height=100, 
+                            bgcolor=ft.colors.WHITE),
+                        crear_container(
+                            ft.ElevatedButton(
+                                "Random",
+                                icon=ft.icons.ALL_INCLUSIVE,
+                                on_click=click_random,
+                                bgcolor='#923fe5',
+                                color='white',
+                                ), 
+                                alignment=ft.alignment.center, 
+                                width=400, 
+                                height=100, 
+                                bgcolor=ft.colors.WHITE),
+                    ],
+                    spacing=20,
+                ),
+                crear_container(
+                    ft.ElevatedButton(
+                    "Convertir", 
+                        on_click=boton_clicked,
+                        bgcolor='#923fe5',
+                        color='white',
+                        ),
+                        alignment=ft.alignment.center, 
+                        width=120, 
+                        height=100, 
+                        bgcolor=ft.colors.WHITE,
+                ),
+                ft.Column(
+                    [
+                        crear_container(
+                            convertir_dropdown, 
+                            alignment=ft.alignment.center, 
+                            width=400, 
+                            height=100, 
+                            bgcolor=ft.colors.WHITE),
+                        crear_container(
+                            convertir_text_field, 
+                            alignment=ft.alignment.center, 
+                            width=400, 
+                            height=100, 
+                            bgcolor=ft.colors.WHITE),
+                        crear_container(
+                             ft.ElevatedButton(
+                                    "Eliminar", 
+                                    icon=ft.icons.RESTORE_FROM_TRASH,
+                                    on_click=eliminar_clicked,
+                                    bgcolor='#923fe5',
+                                    color='white',
+                                ),
+                                alignment=ft.alignment.center, 
+                                width=400, 
+                                height=100, 
+                                bgcolor=ft.colors.WHITE),
+                    ],
+                    spacing=20,
+                ),
+            ],
+            alignment=ft.MainAxisAlignment.CENTER,
+            spacing=50,
+        )
+    )
 
 ft.app(target=main)
